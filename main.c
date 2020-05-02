eeacbd18e0f3d0fa9fb432bce96951ae88ca5c00
Yoo isae <dbdlto@naver.com>
2020-05-02 16:46:53 +0900

    add main function

--- main.c
+++ main.c
@@ -11,3 +11,25 @@ int select_menu(){
 	return menu;
 }
 
+int main(){
+	schedule sch[SIZE];
+	int conut = 0;
+	int curcount =0;
+	while(1){	
+		if(menu == 1){
+			count += add_schedule(&sch[curcount]);
+			curcount++;
+		}
+		else if(menu == 2){
+			if(count == 0){
+				printf("일정이 없습니다.");
+				continue;
+			}
+			read_schedule(sch[curcount]);
+		}
+		else{
+			printf("=======종료=======");
+			break;
+		}		
+	}
+}
